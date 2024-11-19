c2ecf401c0a24edb67e39f5c25e6dec86abab135
Piumi-Anuradha <piumianuradha26@gmail.com>
2024-11-19 06:23:52 +0000

    update routes.py

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,6 +2,17 @@ from flask import render_template
 from taskmanager import app, db
 from taskmanager.models import Category, Recipe
 
+
 @app.route("/")
 def home():
     return render_template("recipes.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_categories", methods=["GET","POST"])
+def category():
+    return render_template("add_categories.html")
